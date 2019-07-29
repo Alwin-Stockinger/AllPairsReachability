4be285d30b1b82ec529ba46758bc6130d1ce75a5
Alwin Stockinger <stockingeralwin@gmail.com>
2019-07-29 18:24:27 +0200

    ecosocial kahip

--- src/partition/Partitioner.cpp
+++ src/partition/Partitioner.cpp
@@ -21,7 +21,7 @@ Partitioner::handlePartitioning(unsigned long long int k, Algora::DiGraph *graph
     pid_t pid;
 
     std::string kahipName = "kaffpa";
-    std::string preconfig = "--preconfiguration=eco";
+    std::string preconfig = "--preconfiguration=ecosocial";
     std::string seed = "--seed=877";
 
     std::string kahipInputFileName = "k";
