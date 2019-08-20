e73b68b2273c9983610471178d4b821cc5d58e98
Srockinger Alwin <a01501158@unet.univie.ac.at>
2019-08-20 17:26:19 +0200

    MeasureAll update

--- measureAll.py
+++ measureAll.py
@@ -4,33 +4,46 @@ import sys
 algorithms = "-A "
 algorithms += "StaticBFS "
 algorithms += "StaticDFS "
-algorithms += "LazyDFS "
-algorithms += "LazyBFS "
-algorithms += "CachingDFS "
-algorithms += "CachingBFS "
-algorithms += "SimpleInc "
-algorithms += "SimpleESTree "
-algorithms += "ESTreeML "
-algorithms += "OldESTree "
-algorithms += "ESTreeQ "
+#algorithms += "LazyDFS "
+#algorithms += "LazyBFS "
+#algorithms += "CachingDFS "
+#algorithms += "CachingBFS "
+#algorithms += "OldESTree "
+#algorithms += "ESTreeQ "
+#algorithms += "ESTreeML "
+#algorithms += "SimpleESTree "
+#algorithms += "SimpleInc "
+
+
+overlayAlgorithm = "-O SimpleInc"
 
 timeOutHours = 0
-timeOutSeconds = 60 * 60 * timeOutHours
+timeOutSeconds = 60 * timeOutHours
 nanoConverter = 1000 * 1000 * 1000  # seconds to nanoseconds
 timeOutNumber = nanoConverter * timeOutSeconds
 timeOut = "-t " + str(timeOutNumber)
 
+percentageTimesNumber = 1
+
 exponentialKNumber = 1
 exponentialK = "--exponentialK " + str(exponentialKNumber)
 
-verticesNumber = 10000
-arcsNumber = 5000
-operationsNumber = 10000
+verticesNumber = 60000
+arcsNumber = 120000
+operationsNumber = 5
 
 minDepthNumber = 0
-maxDepthNumber = 2
+maxDepthNumber = 0
 
-withoutPartitionNumber = 1
+withoutPartitionNumber = 0
+simplePartitionNumber = 0
+superVerticesNumber = 0
+reverseBFSNumber = 0
+BFSONumber = 0
+DFSONumber = 0
+ABFSONumber = 0
+SBFSONumber = 1
+twoWayNumber = 1
 
 multiArcsNumber = 1
 
@@ -38,26 +51,47 @@ queriesNumber = 30
 insertsNumber = 30
 removalNumber = 30
 
-k = "-k 100"
-kMin = "--kMin 2"
+twoWayMin = "--minTwoWay 1000"
+twoWayMax = "--maxTwoWay 1000"
+
+kMax = "-k 33000"
+kMin = "--kMin 4096"
+
+
 vertices = "-s " + str(verticesNumber)
 arcs = "--arcSize " + str(arcsNumber)
 operations = "-o " + str(operationsNumber)
 
+percentageTimes = "--percentageTimes " + str(percentageTimesNumber)
+
 minDepth = "--minDepth " + str(minDepthNumber)
 maxDepth = "--maxDepth " + str(maxDepthNumber)
-withoutPartition = "--withoutPartition " + str(withoutPartitionNumber)
+
+withoutPartition = "--testWithoutPartition " + str(withoutPartitionNumber)
+simplePartition = "--testPartition " + str(simplePartitionNumber)
+superVertices= "--testSuperVertex " + str(superVerticesNumber)
+reverseBFS= "--testReverseBFS " + str(reverseBFSNumber)
+BFSO = "--testBFSO " + str(BFSONumber)
+DFSO = "--testDFSO " + str(DFSONumber)
+ABFSO = "--testAdvancedBFS " + str(ABFSONumber)
+SBFSO = "--testSBFS " + str(SBFSONumber)
+twoWay = "--testTwoWay " + str(twoWayNumber)
+
 multiArcs = "--multiArcs " + str(multiArcsNumber)
 
 queries = "-q " + str(queriesNumber)
 inserts = "-a " + str(insertsNumber)
 removes = "-r " + str(removalNumber)
 
-callString = "nice -n -20 ./AllPairReach"
+repartitionNumber = 0
+repartition = "--repartition " + str(repartitionNumber)
 
-callString += " " + k + " " + algorithms + " " + timeOut \
+callString = "taskset -c 3 ./AllPairReach"
+
+callString += " " + kMax + " " + algorithms + " " + timeOut \
               + " " + kMin + " " + minDepth + " " + maxDepth + " " + withoutPartition + " " + multiArcs \
-              + " " + exponentialK
+              + " " + exponentialK + " " + repartition + " " + overlayAlgorithm + " " + simplePartition + " " + superVertices + " " + ABFSO + " " + SBFSO + " " + reverseBFS + " " + BFSO + " " + percentageTimes + " " + DFSO + " " + twoWay + " " + twoWayMin + " " + twoWayMax
+print("CallString: " + callString)
 
 if len(sys.argv) == 1:
 
