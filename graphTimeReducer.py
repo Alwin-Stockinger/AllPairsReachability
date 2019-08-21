8a70f64be0c17b5874f3fc40444b4a116251b4a7
Alwin Stockinger <stockingeralwin@gmail.com>
2019-08-21 13:02:45 +0200

    results

--- graphTimeReducer.py
+++ graphTimeReducer.py
@@ -1,16 +1,13 @@
 file_name = "origGraph"
-new_file_name = "reducedGraph"
+new_file_name = "smallTestGraph"
 
-endTime = 1230768000  # 2009
+endTime = 1041379200
 
 with open(file_name, "r") as origFile:
     with open(new_file_name, "w") as newFile:
 
         newFile.write(origFile.readline())
         newFile.write(origFile.readline())
-
-        minTime = 1028243088
-        maxTime = 1028243088
 
         for lineStr in origFile:
             splitted_line = lineStr.split(" ")
--- src/handler/AlgorithmTester.cpp
+++ src/handler/AlgorithmTester.cpp
@@ -172,6 +172,8 @@ AlgorithmTester::runTest(DynamicAPReachAlgorithm *algorithm, TimeCollector* time
 
     bool first = true;
 
+    unsigned long long queryCount = 0ULL;
+
     for (const auto &currentQueries : *queries) {
 
         if(first) first = false;
@@ -182,7 +184,7 @@ AlgorithmTester::runTest(DynamicAPReachAlgorithm *algorithm, TimeCollector* time
 
 
             auto startQueryTime = std::chrono::high_resolution_clock::now();
-            algorithm->query(startVertex, endVertex);
+            queryCount += algorithm->query(startVertex, endVertex);
             auto endQueryTime = std::chrono::high_resolution_clock::now();
             timer->addQueryTime(startQueryTime, endQueryTime);
         }
@@ -225,7 +227,7 @@ AlgorithmTester::runTest(DynamicAPReachAlgorithm *algorithm, TimeCollector* time
 
 
 
-    std::cout << "Finished!\n\n" << std::endl;
+    std::cout << "Finished with q= " << queryCount << "\n\n" << std::endl;
 
 }
 
