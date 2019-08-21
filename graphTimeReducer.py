57b253f2ad1b1abbdaac3b76dfd87170199a8775
Srockinger Alwin <a01501158@unet.univie.ac.at>
2019-08-21 16:17:08 +0200

    Synchronize

--- graphTimeReducer.py
+++ graphTimeReducer.py
@@ -1,7 +1,7 @@
 file_name = "origGraph"
-new_file_name = "reducedGraph"
+new_file_name = "reducedNLWiki"
 
-endTime = 1230768000  # 2009
+endTime = 1072915200  # 2004
 
 with open(file_name, "r") as origFile:
     with open(new_file_name, "w") as newFile:
@@ -9,8 +9,6 @@ with open(file_name, "r") as origFile:
         newFile.write(origFile.readline())
         newFile.write(origFile.readline())
 
-        minTime = 1028243088
-        maxTime = 1028243088
 
         for lineStr in origFile:
             splitted_line = lineStr.split(" ")
